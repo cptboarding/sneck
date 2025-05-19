3b2a72ecd2527576bb59fc6676c5e4b461b1a70c
hawks154 <129981503+hawks154@users.noreply.github.com>
2025-05-20 00:53:57 +0200

    Update menu.py
    bug avec generations des murs

--- menu.py
+++ menu.py
@@ -1,55 +1,105 @@
-# -*- coding: utf-8 -*-
-"""Exemple d'utilisation de curses.
- menu du jeu sneck  qui permet d'afficher les differentes options play ,quitter et options. 
-"""
-""" Installation :
-pip install windows-curses 
-pip install curses-menu
-lancer prog : python J:\Informatique\infoOC\sneck\menu.py """
-
-from jeu_snake import programme 
 import subprocess
 import sys
+from cursesmenu import CursesMenu
+from cursesmenu.items import FunctionItem
 import curses
+import jeu_snake
+from murs_qui_bougent import moving_wall  
 
-def play_game():
-    curses.wrapper(programme)
-    """ chemin_script = r"J:\Informatique\infoOC\sneck\jeu_snake.py"
-    print(">>> Lancement du jeu en externe...")
-    result = subprocess.run([sys.executable, chemin_script])
-    print(">>> Jeu terminé avec code retour:", result.returncode)
-    input("Appuyez sur Entrée pour revenir au menu...")"""
+# Mode classique
+def play_game_classic():
+    print(">>> Lancement du jeu classique...")
+    curses.wrapper(jeu_snake.programme)
+    print(">>> Jeu terminé.")
 
-def show_options():
-    print("Options du jeu :")
-    print("1. Mode de base jeu sneck classique avec pomme")
-    print("2. Mode medium (mur en mouvement)")
-    print("3. Mode hardcore (mur en mouvement et pomme qui se tp en touchant le mur)")
-    input("Appuyez sur Entrée pour continuer...")
+# Mode medium avec murs mobiles
+def play_game_medium():
+    print(">>> Lancement du mode medium (murs dynamiques)...")
+
+    def programme_medium(console):
+        murs = [
+            moving_wall((25, 1), 0, 0, (1, 0), 25, 0, x_max=100, y_max=20),
+            moving_wall((25, 1), 25, 0, (1, 0), 25, 0, x_max=100, y_max=20),#ici que leprogramme bug avec les dimensions
+            moving_wall((25, 1), 50, 0, (1, 0), 25, 0, x_max=100, y_max=20),
+            moving_wall((25, 1), 75, 0, (1, 0), 25, 0, x_max=100, y_max=20),
+
+            moving_wall((25, 1), 0, 20, (1, 0), 25, 0, x_max=100, y_max=20),
+            moving_wall((25, 1), 25, 20, (1, 0), 25, 0, x_max=100, y_max=20),
+            moving_wall((25, 1), 50, 20, (1, 0), 25, 0, x_max=100, y_max=20),
+            moving_wall((25, 1), 75, 20, (1, 0), 25, 0, x_max=100, y_max=20),
+
+            moving_wall((1, 5), 0, 0, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 0, 5, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 0, 10, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 0, 15, (0, 1), 20, 0, x_max=100, y_max=20),
+
+            moving_wall((1, 5), 100, 0, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 100, 5, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 100, 10, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 100, 15, (0, 1), 20, 0, x_max=100, y_max=20),
+        ]
+        jeu_snake.programme(console, murs=murs)
+
+    curses.wrapper(programme_medium)
+    print(">>> Jeu terminé.")
+def play_game_hardcore():
+    print(">>> Lancement du mode hardcore (murs dynamiques + pommes qui se téléportent)...")
+
+    def programme_hardcore(console):
+        murs = [
+            moving_wall((25, 1), 0, 0, (1, 0), 25, 0, x_max=100, y_max=20),
+            moving_wall((25, 1), 25, 0, (1, 0), 25, 0, x_max=100, y_max=20),#ici que leprogramme bug avec les dimensions
+            moving_wall((25, 1), 50, 0, (1, 0), 25, 0, x_max=100, y_max=20),
+            moving_wall((25, 1), 75, 0, (1, 0), 25, 0, x_max=100, y_max=20),
+
+            moving_wall((25, 1), 0, 20, (1, 0), 25, 0, x_max=100, y_max=20),
+            moving_wall((25, 1), 25, 20, (1, 0), 25, 0, x_max=100, y_max=20),
+            moving_wall((25, 1), 50, 20, (1, 0), 25, 0, x_max=100, y_max=20),
+            moving_wall((25, 1), 75, 20, (1, 0), 25, 0, x_max=100, y_max=20),
+
+            moving_wall((1, 5), 0, 0, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 0, 5, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 0, 10, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 0, 15, (0, 1), 20, 0, x_max=100, y_max=20),
+
+            moving_wall((1, 5), 100, 0, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 100, 5, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 100, 10, (0, 1), 20, 0, x_max=100, y_max=20),
+            moving_wall((1, 5), 100, 15, (0, 1), 20, 0, x_max=100, y_max=20),
+            
+        ]
+        jeu_snake.programme(console, murs=murs, pomme_tp=True)  # Il faut rajouter les pommes qui tps
+
+    curses.wrapper(programme_hardcore)
+    print(">>> Jeu terminé.")
+
+
+
 
 def quit_game():
     print("Quitter le jeu...")
-    sys.exit()
+    exit()
 
+# Fonction principale du menu
 def main():
-    while True:
-        print("\n--- Menu Sneck Game ---")
-        print("1. Jouer")
-        print("2. Options")
-        print("3. Quitter")
-        choix = input("Votre choix : ")
-        if choix == "1":
-            play_game()
-        elif choix == "2":
-            show_options()
-        elif choix == "3":
-            quit_game()
-        else:
-            print("Choix invalide, réessayez.")
+    menu = CursesMenu("Sneck Game", "Utilisez les flèches pour naviguer")
+
+    play_classic = FunctionItem("Jouer (Classique)", play_game_classic)
+    play_medium = FunctionItem("Jouer (Medium - murs dynamiques)", play_game_medium)
+    play_hardcore = FunctionItem("Jouer (Hardcore - murs + pommes dynamiques)", play_game_hardcore)
+    options_item = FunctionItem("Options", show_options)
+    quit_item = FunctionItem("Quitter", quit_game)
+
+    menu.items.append(play_classic)
+    menu.items.append(play_medium)
+    menu.items.append(play_hardcore)
+    menu.items.append(options_item)
+    menu.items.append(quit_item)
+
+    menu.show()
 
 if __name__ == "__main__":
     main()
-
 
 
 
