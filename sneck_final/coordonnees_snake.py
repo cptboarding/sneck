53289a99c61fc1e4a38c0285e7442aacd1ed6be6
EverEvolution <alexisblaudszun@gmail.com>
2025-05-20 18:10:00 +0200

    Add files via upload

---
+++ sneck_final/coordonnees_snake.py
@@ -0,0 +1,89 @@
+"""
+Module qui permet de créer des instances de la classe personnage, qui peut changer de coordonnées et
+avoir des clones qui le suivent.
+"""
+
+from typing import NamedTuple
+
+
+class personnage(NamedTuple):
+    y: int
+    x: int
+    caractere_snake: str
+    clones: list
+    caractere_clones: str
+    
+    
+    @staticmethod
+    def verifier(y, x, clones,  x_min, y_min, x_max, y_max):
+        """Vérifie si les coordonnées y et x sont possibles"""
+        if (y, x) in clones:
+            return "nul"
+        elif x == x_min or x == x_max or y == y_min or y == y_max:
+            return "nul"
+
+    
+    def nouvelles_coordonnees(self, key, x_min, y_min, x_max, y_max):
+        """Retourne les nouvelles coordonnées en fonction du key"""
+        if key == "a":
+            if personnage.verifier(self.y, self.x-1, self.clones, x_min, y_min, x_max, y_max) == "nul":
+                return "nul"
+            else:
+                x = self.x-1
+                y = self.y
+        if key == "d":
+            if personnage.verifier(self.y, self.x+1, self.clones, x_min, y_min, x_max, y_max) == "nul":
+                return "nul"
+            else:
+                x = self.x+1
+                y = self.y
+        if key == "w":
+            if personnage.verifier(self.y-1, self.x, self.clones, x_min, y_min, x_max, y_max) == "nul":
+                return "nul"
+            else:
+                y = self.y-1
+                x = self.x
+        if key == "s":
+            if personnage.verifier(self.y+1, self.x, self.clones, x_min, y_min, x_max, y_max) == "nul":
+                return "nul"
+            else:
+                y = self.y+1
+                x = self.x
+        return y, x
+     
+    @staticmethod
+    def supprimer_premier_element(collection):
+        """Supprime le premier élément d'une collection"""
+        nouvelle_collection = []
+        for i in range(len(collection)):
+            if i != 0:
+                nouvelle_collection.append(collection[i])
+        return nouvelle_collection
+
+
+    def update_clones(self, longueur_max):
+        """Supprimme le dernier clone si la longueur_max est atteinte, et ajoute un nouveau clone"""
+        clones = self.clones
+        if len(clones) == longueur_max:
+            clones = personnage.supprimer_premier_element(clones)
+        if longueur_max > 0: #s'il y a au moins un clone à afficher
+            clones.append((self.y, self.x))
+        return clones
+
+
+def _tests():
+    snake_exemple = personnage(10, 10, "@", [(10, 7), (10, 8), (10, 9)], "#")
+    assert snake_exemple.nouvelles_coordonnees("d", 0, 0, 20, 20) == (10, 11)
+
+    nouveaux_clones = snake_exemple.update_clones(longueur_max=3)
+    y, x = snake_exemple.nouvelles_coordonnees("d", 0, 0, 20, 20)
+    snake_exemple_2 = personnage(y, x, "@", nouveaux_clones, "#")
+    assert (snake_exemple_2.y, snake_exemple_2.x)  == (10, 11)
+    assert snake_exemple_2.clones == [(10, 8), (10, 9), (10, 10)]
+
+    print("Tests réussis")
+
+
+if __name__ == "__main__":
+    _tests()
+
---
+++ sneck_final/snake.py
@@ -0,0 +1,128 @@
+"""
+Module qui affiche un snake qui se déplace sur une fenêtre de curses.
+
+python "J:\Documents\Informatique\3ème année\Projet 2ème semestre\snake_16052025\snake.py"
+"""
+
+import curses
+import random
+import time
+
+from coordonnees_snake import personnage
+from murs_qui_bougent import moving_wall
+
+RIEN = " "
+PERSONNAGE = "@"
+CLONE = "#"
+BORDER = "-"
+
+
+def debut(console):
+    console.clear()
+    curses.curs_set(False)
+    console.refresh()
+
+
+def afficher(console, texte, y_texte, x_texte, temps=5):
+    """Affiche du texte"""
+    for lettre in texte:
+        console.addstr(y_texte, x_texte, lettre)
+        x_texte = x_texte+1
+    console.getkey()
+    for lettre in texte:
+        x_texte = x_texte-1
+    for lettre in texte:
+        console.addstr(y_texte, x_texte, RIEN)
+        x_texte = x_texte+1
+
+
+def border(console, caractere, x_min, y_min, x_max, y_max):
+    """Dessine les bords"""
+    for y in range(y_min, y_max+1):
+        console.addstr(y, x_min, caractere)
+        console.addstr(y, x_max, caractere)
+    for x in range(x_min, x_max+1):
+        console.addstr(y_max, x, caractere)
+        console.addstr(y_min, x, caractere)
+
+
+def game_over(console, snake, x_min, y_min, x_max, y_max):
+    """Supprime tout et affiche "tu es nul" """
+    console.addstr(snake.y, snake.x, RIEN)
+    for coordonnees in snake.clones:
+        console.addstr(coordonnees[0], coordonnees[1], RIEN)
+    for y in range(y_min, y_max+1):
+        console.addstr(y, x_min, RIEN)
+        console.addstr(y, x_max, RIEN)
+    for x in range(x_min, x_max+1):
+        console.addstr(y_max, x, RIEN)
+        console.addstr(y_min, x, RIEN)
+    afficher(console, "Game over", 10, 50)
+
+
+def frame(console, snake, longueur_max, x_min, y_min, x_max, y_max, key):
+    """Affiche le snake"""
+    clones = snake.clones
+
+    console.addstr(snake.y, snake.x, snake.caractere_snake) #affiche la tête
+    console.nodelay(True)
+    try:
+        newkey = console.getkey()
+        if newkey == key or (newkey == 'a' and key == 'd') or (newkey == 'd' and key == 'a') or (newkey == 'w' and key == 's') or (newkey == 's' and key == 'w'):
+            pass
+        else:
+            key = newkey
+    except:
+        pass
+
+    if len(snake.clones) == longueur_max and len(snake.clones)>0:
+        console.addstr(snake.clones[0][0], snake.clones[0][1], RIEN) #affiche rien au dernier clone
+    clones = snake.update_clones(longueur_max) #change les clones
+    if len(snake.clones)>0:
+        console.addstr(snake.y, snake.x, snake.caractere_clones) #affiche le nouveau clone derrière la tête
+    else:
+        console.addstr(snake.y, snake.x, RIEN) #s'il n'y a pas de clones, on affiche rien derrière la tête
+        
+    if snake.nouvelles_coordonnees(key, x_min, y_min, x_max, y_max) == "nul":
+        return 'c est nul'
+    else:
+        y, x = snake.nouvelles_coordonnees(key,  x_min, y_min, x_max, y_max)
+        return y, x, clones, key
+
+
+def jeu(console):
+    """Le jeu"""
+    hauteur, longueur = console.getmaxyx()
+    x_min, y_min, x_max, y_max = 10, 5, longueur-10, hauteur-5
+    x, y = 30, 15
+    score = 0
+    longueur_max = 0
+    key = "d"
+    vitesse_initiale = 5
+
+    clones = []
+
+    border(console, BORDER, x_min, y_min, x_max, y_max)
+
+    while True:
+        longueur_max = score//5
+        vitesse = vitesse_initiale + score/10
+        snake = personnage(y, x, PERSONNAGE, clones, CLONE)
+        time.sleep(1/vitesse)
+        f = frame(console, snake, longueur_max, x_min, y_min, x_max, y_max, key)
+        if f == 'c est nul':
+            break
+        else:
+            y, x, clones, key = f
+
+    console.nodelay(False)
+    game_over(console, snake, x_min, y_min, x_max, y_max)
+
+
+def programme(console):
+    debut(console)
+    jeu(console)
+
+
+if __name__ == "__main__":
+    curses.wrapper(programme)
